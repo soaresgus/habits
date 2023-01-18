c2fcbf5e618e1b738574e34605cc9d31ee9ba872
soaresgus <gustavosoaresbt@hotmail.com>
2023-01-18 07:16:06 -0300

    Create mobile setup

---
+++ mobile/App.tsx
@@ -0,0 +1,47 @@
+import { useCallback } from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from '@expo-google-fonts/inter';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text>Hello world!</Text>
+      <StatusBar barStyle="default" backgroundColor="transparent" translucent />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7f7f7f',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
