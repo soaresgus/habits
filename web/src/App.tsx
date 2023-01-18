1fb5272e82a79345a80b9032b9fadb166262fb8f
soaresgus <gustavosoaresbt@hotmail.com>
2023-01-18 18:20:30 -0300

    Create web home page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,14 @@
+import { Header } from './components/Header';
+import { SummaryTable } from './components/SummaryTable';
 import './styles/global.css';
 
-function App() {
-  return <h1>Hello world!</h1>;
+export function App() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <main className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </main>
+    </div>
+  );
 }
-
-export default App;
