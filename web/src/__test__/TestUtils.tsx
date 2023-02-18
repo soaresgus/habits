38fa4dc3b598214228c00fd6b5b6a438c3bb5226
soaresgus <gustavosoaresbt@hotmail.com>
2023-02-18 12:58:29 -0300

    test(web): Create test utils file

---
+++ web/src/__test__/TestUtils.tsx
@@ -0,0 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { render, RenderOptions } from '@testing-library/react';
+
+import { SearchProvider } from '../context/SearchContext';
+
+export function AllWrappers({ children }: { children: React.ReactNode }) {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SearchProvider>{children}</SearchProvider>
+    </QueryClientProvider>
+  );
+}
+
+function customRender(ui: React.ReactElement, options?: RenderOptions) {
+  return render(ui, { wrapper: AllWrappers, ...options });
+}
+
+export * from '@testing-library/react';
+export { customRender as render };
